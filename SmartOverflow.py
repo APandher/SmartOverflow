4d9a004a6bf264ab4a9ed52f5036c0117c1881ed
Pandher <pandha1@mcmaster.ca>
2020-02-22 19:41:46 -0500

    Fixing class definition

--- SmartOverflow.py
+++ SmartOverflow.py
@@ -1,16 +1,26 @@
 import googlesearch
 import os
 
-error = ""
-URL = error
-count = 0 
+class SmartOverflow:
 
-for j in googlesearch.search(URL, stop=10):
-    if "stackoverflow" in j and count < 3:
-        print(j)
-        count += 1
+    def __init__(self):
+        try:
+
+        except exception as x:
+            smart_except(x)
+            
     
+    def smart_except(self,x):
+   
+        error = x
+        URL = str(error)
+        count = 0 
 
+        for j in googlesearch.search(URL, stop=10):
+            if "stackoverflow" in j and count < 3:
+                print(j)
+                count += 1
+ 
 
 
 
